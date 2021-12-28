eb8450e571447b2a10c02ff78a96e695201dd1f3
Documenter.jl <documenter@juliadocs.github.io>
2021-12-28 02:25:26 +0000

    build based on 178a34f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Welcome-to-the-LuxCat.jl-Docs!","page":"Home","title":"Welcome to the LuxCat.jl Docs! 🐱","text":"","category":"section"},{"location":"#Installing-LuxCat.jl","page":"Home","title":"Installing LuxCat.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"To install LuxCat into your Julia installation, type into your Julia REPL the following:","category":"page"},{"location":"","page":"Home","title":"Home","text":"julia> ] add LuxCat","category":"page"}]
+}
